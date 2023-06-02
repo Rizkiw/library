223a58f4410e2b55aec7e9b01e2416db00bd72dc
Rizki Wibisono <rizki.wibisono.k@gmail.com>
2023-06-02 20:14:54 +0700

    create route for get user by id

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getUser } from "../controllers/UserController.js";
+import { 
+    getUser,
+    getUserById
+  } from "../controllers/UserController.js";
 
 const router = express.Router();
 
+router.get('/users', getUser);
 router.get('/users', getUser);
 
 export default router;
