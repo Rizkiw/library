adfe75d00a3026b06c3fc9312f11af30eea21618
Rizki Wibisono <rizki.wibisono.k@gmail.com>
2023-06-02 19:59:59 +0700

    create model for database

---
+++ backend/models/UserModel.js
@@ -0,0 +1,18 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+
+const {DataTypes} = Sequelize;
+
+const Book = db.define('books', {
+    title: DataTypes.STRING,
+    genre: DataTypes.STRING,
+    author: DataTypes.STRING,
+    year: DataTypes.STRING
+}, {
+    freezeTableName : true
+});
+
+export default Book;
+(async()=>{
+    await db.sync();
+})();
